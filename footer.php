209b3eabf9bf9457ceb5f253e92e8f96b46412de
Paulo Victor dos Santos Barbosa <120179816+Paulo-VictorSB@users.noreply.github.com>
2025-02-07 08:07:36 -0300

    Arquivos de estruturação adicionados.

---
+++ footer.php
@@ -0,0 +1,26 @@
+            </div>
+        </div>
+    </div>
+    
+    <!-- footer -->
+    <div class="container-fluid fixed-bottom">
+        <div class="row">
+            <div class="col bg-dark text-white p-2 text-center">
+                <p>PAULO VICTOR &copy; <?= date('Y') ?></p>
+                <a href="eliminar_tudo.php">Eliminar tudo</a>
+                <span class="mx-2">|</span>
+                <a href="exportar_csv.php">Exportar para CSV</a>
+                <span class="mx-2">|</span>
+                <a href="https://www.linkedin.com/in/paulo-barbosa-77199925a/" target="_blank">Linkedin</a>
+                <span class="mx-2">|</span>
+                <a href="https://github.com/Paulo-VictorSB" target="_blank">Github</a>
+                <span class="mx-2">|</span>
+            </div>
+        </div>
+    </div> 
+    
+    <!-- Bootstrap JS and custom JS -->
+    <script src="assets/bootstrap.bundle.min.js"></script>
+    <script src="assets/app.js"></script>
+</body>
+</html>
